6ca7cbce6f0c5856257191817cf902afb5a69c96
Adhityae-506 <adhityaelangovan2006@gmail.com>
2024-09-08 16:28:50 +0530

    created Hello.cpp file

---
+++ Hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "************************";
+    cout << "Hello world!!" <<endl;
+    cout << "************************";
+
+    return 0;
+    
+}
